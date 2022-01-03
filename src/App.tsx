4a87e1a419368ef5b36c30d5824ae576041d98ee
VietAnh Dam <anhdam.tdt.asia>
2022-01-03 17:55:09 +0700

    imple header page

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import {store, persistor} from './redux/store/configureStore';
 import { PersistGate } from 'redux-persist/integration/react';
 import {Provider} from 'react-redux';
 import RouterDom from './Router';
+import Header from './components/Header';
+// import './components/Header/Header.css'
 
 const App = () => {
   return (
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+      <Header />
       <RouterDom/>
     </PersistGate>
   </Provider>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,12 +8,31 @@ const Header = () => {
     <div className="header-top">
       <div className="container-fluid d-flex justify-content-end">
         <ul className="nav header-top-nav">
-          <li className="nav-item"><a href="#" className="nav-link"><i className="bi bi-arrow-bar-left"></i></a></li>
-          <li className="nav-item"><a href="#" className="nav-link"><i className="bi bi-arrows-angle-contract"></i></a></li>
-          <li className="nav-item item-menu"><a href="#" className="nav-link"><i className="bi bi-list"></i></a></li>
-          <li className="nav-item item-notication"><a href="#" className="nav-link"><i className="bi bi-bell-fill"></i><sup className="count">04</sup></a></li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z" />
+              </svg>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link pl-0">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrows-angle-expand" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z" />
+              </svg>
+            </a>
+          </li>
+          <li className="nav-item item-menu"><a href="#" className="nav-link pl-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list-nested" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5z" />
+            </svg></a></li>
+          <li className="nav-item item-notication"><a href="#" className="nav-link pl-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bell-fill" viewBox="0 0 16 16">
+              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
+            </svg>
+            <sup className="count">04</sup></a></li>
           <li className="nav-item dropdown">
-            <a href="#" className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">C123466</a>
+            <a href="#" className="nav-link dropdown-toggle pl-0" role="button" data-bs-toggle="dropdown" aria-expanded="false">C123466</a>
             <ul className="dropdown-menu dropdown-menu-end">
               <li><a className="dropdown-item" href="#">Sub Menu</a></li>
               <li><a className="dropdown-item" href="#">Logout</a></li>
@@ -26,31 +45,61 @@ const Header = () => {
 
   const setHeaderMain = () => (
     <div className="header-main">
-        <div className="container d-flex align-items-end">
-          <div className="site-brand">
-            <h1 className="site-title mb-0">
-              <a href="index.html" className="site-link"><img src={LOGO_ICON} className="site-logo" alt="" /></a>
-            </h1>
-          </div>
-          <ul className="nav header-nav">
-            <li className="nav-item item-dashboard"><a href="index.html" className="nav-link active"><i className="icon bi bi-app-indicator me-1"></i><span className="d-none d-lg-inline-block">Dashboard</span></a></li>
-            <li className="nav-item item-news"><a href="news.html" className="nav-link"><i className="icon bi bi-card-text me-1"></i><span className="d-none d-lg-inline-block">News</span></a></li>
-            <li className="nav-item item-order dropdown">
-              <a href="#" className="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="icon bi bi-clipboard me-1"></i><span className="d-none d-lg-inline-block">Order</span></a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item item-order-Monitoring" href="order-monitoring.html">Order Monitoring</a></li>
-                <li><a className="dropdown-item item-order-history" href="order-history.html">Order History</a></li>
-                <li><a className="dropdown-item item-trade-history" href="trade-history.html">Trade History</a></li>
-                <li><a className="dropdown-item item-order-portfolio" href="order-portfolio.html">Portfolio</a></li>
-                <li><a className="dropdown-item item-order-new" href="new-order.html">New</a></li>
-                <li><a className="dropdown-item item-order-modify-cancel" href="order-modify-cancel.html">Modify - Cancel Order</a></li>
-              </ul>
-            </li>
-            <li className="nav-item item-customer"><a href="#" className="nav-link"><i className="icon bi bi-person-workspace me-1"></i><span className="d-none d-lg-inline-block">Customer Infomation</span></a></li>
-            <li className="nav-item item-report"><a href="report.html" className="nav-link"><i className="icon bi bi-clipboard-data me-1"></i><span className="d-none d-lg-inline-block">Report</span></a></li>
-          </ul>
+      <div className="container d-flex align-items-end">
+        <div className="site-brand">
+          <h1 className="site-title mb-0">
+            <a href="" className="site-link">
+              <img src={LOGO_ICON.default} />
+            </a>
+          </h1>
         </div>
+        <ul className="nav header-nav">
+          <li className="nav-item item-dashboard"><a href="/" className="nav-link active">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-app-indicator" viewBox="0 0 16 16">
+              <path d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1H5.5z" />
+              <path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+            </svg>
+            <span className="d-none d-lg-inline-block ml-5">Dashboard</span></a></li>
+          <li className="nav-item item-news">
+            <a href="/" className="nav-link color-not-active">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-card-text" viewBox="0 0 16 16">
+                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
+              </svg>
+              <span className="d-none d-lg-inline-block ml-5">News</span></a></li>
+          <li className="nav-item item-order dropdown">
+            <a className="nav-link dropdown-toggle color-not-active" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard-check" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+              </svg>
+              <span className="d-none d-lg-inline-block ml-5">Order</span></a>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a className="dropdown-item item-order-Monitoring" href="/orders/monitoring">Order Monitoring</a></li>
+              <li><a className="dropdown-item item-order-history" href="/orders/history">Order History</a></li>
+              <li><a className="dropdown-item item-trade-history" href="trade-history.html">Trade History</a></li>
+              <li><a className="dropdown-item item-order-portfolio" href="order-portfolio.html">Portfolio</a></li>
+              <li><a className="dropdown-item item-order-new" href="new-order.html">New</a></li>
+              <li><a className="dropdown-item item-order-modify-cancel" href="order-modify-cancel.html">Modify - Cancel Order</a></li>
+            </ul>
+          </li>
+          <li className="nav-item item-customer color-not-active"><a href="#" className="nav-link color-not-active">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-workspace" viewBox="0 0 16 16">
+              <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+              <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z" />
+            </svg>
+            <span className="d-none d-lg-inline-block ml-5">Customer Infomation</span></a></li>
+          <li className="nav-item item-report color-not-active"><a href="/" className="nav-link color-not-active">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard-data" viewBox="0 0 16 16">
+              <path d="M4 11a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0v-1zm6-4a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0V7zM7 9a1 1 0 0 1 2 0v3a1 1 0 1 1-2 0V9z" />
+              <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+              <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+            </svg>
+            <span className="d-none d-lg-inline-block ml-5">Report</span></a></li>
+        </ul>
       </div>
+    </div>
   )
 
   const setHeaderTemplate = () => (
