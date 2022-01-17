7a4cf1735106767a177cf31ac84c924b9badf122
oanhdang <oanhdang@tdt.asia>
2022-01-17 10:19:08 +0700

    fix comment

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -1,20 +1,25 @@
 import moment from 'moment';
-import { FORMAT_DATE_TIME, INVALID_DATE } from '../constants/general.constant';
+import { FORMAT_DATE_TIME_MILLI, INVALID_DATE } from '../constants/general.constant';
 
 export function formatOrderTime(date: number): string {
-    // time in miliseconds
-    const dateTime = moment(date).format(FORMAT_DATE_TIME);
+    // time
+    const dateTime = moment(date).format(FORMAT_DATE_TIME_MILLI);
     if (dateTime !== INVALID_DATE) {
         return dateTime;
     }
     return '';
 }
+
 export function calcPendingVolume(volume: string, filledAmount: string) {
     return Number(volume) - Number(filledAmount);
 }
+
+// To format volume.
 export function formatNumber(item: string): string {
-    return new Intl.NumberFormat('en-US').format(Number(Number(item)));
+    return new Intl.NumberFormat('en-US').format(Number(item));
 }
+
+// To format price --after the dot is 2 decimals.
 export function formatCurrency(item: string): string {
     return new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(Number(Number(item).toFixed(2)));
 }
