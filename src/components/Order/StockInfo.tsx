242a1d1706288e77097198bb0a3c4638692ec836
oanhdang <oanhdang@tdt.asia>
2022-01-24 16:32:27 +0700

    change font-size of text

--- src/components/Order/StockInfo.tsx
+++ src/components/Order/StockInfo.tsx
@@ -9,23 +9,23 @@ const StockInfo = () => {
                                     <table className="table table-sm mb-0" cellSpacing="0" cellPadding="0">
                                         <tbody>
                                             <tr>
-                                                <th>Mininum bid size</th>
+                                                <th className="fs-14">Mininum bid size</th>
                                                 <td className="text-end fw-600">0.01</td>
                                             </tr>
                                             <tr>
-                                                <th>Lot size</th>
+                                                <th className="fs-14">Lot size</th>
                                                 <td className="text-end fw-600">0.01</td>
                                             </tr>
                                             <tr>
-                                                <th>Volume</th>
+                                                <th className="fs-14">Volume</th>
                                                 <td className="text-end fw-600">1,267,500</td>
                                             </tr>
                                             <tr>
-                                                <th>52w High</th>
+                                                <th className="fs-14">52w High</th>
                                                 <td className="text-end fw-600">174.34</td>
                                             </tr>
                                             <tr>
-                                                <th>52w Low</th>
+                                                <th className="fs-14">52w Low</th>
                                                 <td className="text-end fw-600">142.89</td>
                                             </tr>
                                         </tbody>
