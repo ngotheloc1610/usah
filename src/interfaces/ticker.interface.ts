4fd321022dd37a5e2c06c0c1e132522ca800a140
linhha <linhha@tdt.asia>
2022-02-07 19:45:58 +0700

    48918-fix code convention in interface

--- src/interfaces/ticker.interface.ts
+++ src/interfaces/ticker.interface.ts
@@ -1,45 +1,45 @@
 export interface ITickerDetail {
-    symbolId: number,
-    tickerName: string,
-    ticker: string,
-    stockPrice: string,
-    previousClose: string,
-    open: string,
-    high: string,
-    low: string,
-    lastPrice: string,
-    volume: string,
-    change: string,
-    changePrecent: string,
-    lotSize: string,
+    symbolId: number;
+    tickerName: string;
+    ticker: string;
+    stockPrice: string;
+    previousClose: string;
+    open: string;
+    high: string;
+    low: string;
+    lastPrice: string;
+    volume: string;
+    change: string;
+    changePrecent: string;
+    lotSize: string;
     minimumBizSize: string
 }
 
 export interface ISymbolList {
-    calculationMode: number,
-    contractSize: number,
-    currencyCode: string,
-    description: string,
-    digits: number,
-    exchange: string,
-    lotSize: string,
-    minLot: string,
-    symbolCode: string,
-    symbolId: number,
-    symbolName: string,
+    calculationMode: number;
+    contractSize: number;
+    currencyCode: string;
+    description: string;
+    digits: number;
+    exchange: string;
+    lotSize: string;
+    minLot: string;
+    symbolCode: string;
+    symbolId: number;
+    symbolName: string;
     tickSize: string
 }
 
 export interface IListDashboard {
-    symbolName: string,
-    symbolCode: string,
-    previousClose?: string,
-    open?: string,
-    high?: string,
-    low?: string,
-    lastPrice?: string,
-    volume?: string,
-    change: number,
-    percentChange: number,
+    symbolName: string;
+    symbolCode: string;
+    previousClose?: string;
+    open?: string;
+    high?: string;
+    low?: string;
+    lastPrice?: string;
+    volume?: string;
+    change: number;
+    percentChange: number;
 
 }
