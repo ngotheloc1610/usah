ce094a57a6075e97c38dc1960ade8d61974b613a
linhha <linhha@tdt.asia>
2022-02-14 11:45:42 +0700

    48983-update logic trading pin flg

--- src/constants/general.constant.ts
+++ src/constants/general.constant.ts
@@ -63,7 +63,7 @@ export const TITLE_CONFIRM = {
 };
 
 export const ORDER_TYPE = [
-    { id: 1, name: 'Limit'}
+    { id: 1, name: 'Limit' }
 ];
 
 export const ORDER_TYPE_NAME = {
@@ -103,3 +103,5 @@ export enum MESSAGE_TOAST {
 };
 
 export const CURRENT_CHOOSE_TICKER = 'tickerDetail';
+
+export const IS_ACTIVE_TRADING_PIN = 'isActiveTradingPin';
