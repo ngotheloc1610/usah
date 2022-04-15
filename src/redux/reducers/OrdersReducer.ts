89197f2eafd3442768df7ce164a04a1946b9e703
linhha <linhha@tdt.asia>
2022-04-15 08:53:02 +0700

    52904-format code

--- src/redux/reducers/OrdersReducer.ts
+++ src/redux/reducers/OrdersReducer.ts
@@ -3,21 +3,20 @@ import Types from "../types";
 
 const initState = {
     listOrderMultiOrder: []
-  };
-  
-  const OrdersReducer = (state = initState, action: AnyAction) => {
-    const {type, payload} = action;
+};
+
+const OrdersReducer = (state = initState, action: AnyAction) => {
+    const { type, payload } = action;
     switch (type) {
-      case Types.LIST_ORDER_MULTI_ORDER:
-        return {
-          ...state,
-          listOrderMultiOrder: payload
-        };
-  
-      default:
-        return state;
+        case Types.LIST_ORDER_MULTI_ORDER:
+            return {
+                ...state,
+                listOrderMultiOrder: payload
+            };
+
+        default:
+            return state;
     }
-  };
-  
-  export default OrdersReducer;
-  
+};
+
+export default OrdersReducer;
