5294264c2649014d1b0798cb6df0ecdd6f0acae1
oanhdang <oanhdang@tdt.asia>
2022-05-05 17:26:55 +0700

    54245 Number of unread news is incorrect

--- src/components/Modal/PopUpNotification/index.tsx
+++ src/components/Modal/PopUpNotification/index.tsx
@@ -35,28 +35,28 @@ const PopUpNotification = (props: IPopsNotification) => {
      const convertTime = (item: string) => {
           return moment(item).format(FORMAT_DATE_TIME_MILLIS)
      }
-     const _renderTradingResultsItem = () => (
-          listTradingResults?.map((item: ITradingResult, idx: number) => (
-               <div className={!item.readFlg ? "notification-item unread" : "notification-item"
-                    && elTradingActive === idx ? "notification-item active" : "notification-item"}
-                    key={idx}
-                    onClick={() => handleClickTradingResult(item, idx)}
-               >
-                    <div className="item-icon">
-                         <i className="bi bi-cash-stack"></i>
+    const _renderTradingResultsItem = () => (
+        listTradingResults?.map((item: ITradingResult, idx: number) => (
+            <div className={!item.readFlg ? "notification-item unread" : "notification-item"
+                && elTradingActive === idx ? "notification-item active" : "notification-item"}
+                key={idx}
+                onClick={() => handleClickTradingResult(item, idx)}
+            >
+                <div className="item-icon">
+                    <i className="bi bi-cash-stack"></i>
+                </div>
+                <div className="item-content">
+                    <h5 className="item-title mb-0">Trading Results Information</h5>
+                    <div className="item-summary opacity-75 fix-line-css">
+                        {getSideName(Number(item.orderSide))} {item.execVolume} {item.symbolCode} price {item.execPrice.toFixed(2)}
                     </div>
-                    <div className="item-content">
-                         <h5 className="item-title mb-0">Trading Results Information</h5>
-                         <div className="item-summary opacity-75 fix-line-css">
-                              {getSideName(Number(item.orderSide))} {item.execVolume} {item.symbolCode} price {item.execPrice.toFixed(2)}
-                         </div>
-                         <div className="item-summary opacity-75 fix-line-css">
-                              {moment(item.execTime).format(FORMAT_DATE_NEW_OR_RESULT)}
-                         </div>
+                    <div className="item-summary opacity-75 fix-line-css">
+                        {moment(item.execTime).format(FORMAT_DATE_NEW_OR_RESULT)}
                     </div>
-               </div>
-          ))
-     )
+                </div>
+            </div>
+        ))
+    )
      
      const getDataTradingResult = () => {
           const paramTrading = {   
@@ -66,8 +66,10 @@ const PopUpNotification = (props: IPopsNotification) => {
           axios.get<IReqTradingResult, IReqTradingResult>(urlGetTradingResult, defindConfigGet(paramTrading)).then((resp) => {
           if (resp.status === success) {
                if(resp?.data?.data) {
+                    if (currentPageTrading !== FIRST_PAGE) {
+                         setListTradingResults(prev => [...prev,...resp?.data?.data?.results]);
+                    }
                     const nextPage = resp?.data?.data?.next_page;
-                    setListTradingResults(prev => [...prev,...resp?.data?.data?.results]);
                     setIsLastPage(nextPage === currentPageTrading);
                     setCurrentPageTrading(currentPageTrading + 1)
                }
