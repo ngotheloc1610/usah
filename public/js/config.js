bb765d264bdb81cd7e9f7bbaf147f2bdabda5676
VietAnh Dam <anhdam.tdt.asia>
2022-07-14 15:30:56 +0700

    [Session - LP] Apply session time out after 1hrs of idle

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,7 @@
 window.globalThis = {
     "wsUrl": "wss://pixstg.tdt.asia/ws/api/v1/websocket",
     "apiUrl": "https://pixstg.tdt.asia/api",
-    "flagRmsApi": "true"
+    "flagRmsApi": "true",
+    "idleTime": 1000,
+    "sessionTimeOut": 3600000
 }
--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,36 @@ const App = () => {
   const [isLogin, setIsLogin] = useState(false);
   const [isResetPassword, setIsResetPassword] = useState(false);
   const [isForgotPassword, setIsForgotPassword] = useState(false);
+  const [idleTime, setIdleTime] = useState(0);
+  const [isShowIdleTimeOut, setIsShowIdleTimeOut] = useState(false);
+
+  const idleEvents = ['load', 'mousemove', 'mousedown', 'click', 'scroll', 'keypress'];
+
+   // Increment the idle time counter every minute.
+   useEffect(() => {
+    const interval = setInterval(() => {
+      setIdleTime(prev => prev + 1)
+    }, window.globalThis.idleTime);
+    return () => clearInterval(interval);
+  }, []);
+
+  // Set the idle timer to 0 if catch event.
+  useEffect(() => {
+    idleEvents.forEach(event => {
+      window.addEventListener(event, () => setIdleTime(0));
+    })
+    return () => {
+      idleEvents.forEach(event => {
+        window.removeEventListener(event, () => setIdleTime(0));
+      })
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!window.location.pathname.includes('/login') && window.globalThis.sessionTimeOut < (idleTime * 1000)) {
+      setIsShowIdleTimeOut(true);
+    }
+  }, [idleTime])
 
   useEffect(() => {
     checkLoginPage();
@@ -75,7 +105,29 @@ const App = () => {
       </>
     )
   }
+
+ const gotoLoginPage = () => {
+  setIsShowIdleTimeOut(false);
+  window.location.href = '/login';
+ }
+
+  const renderIdleTimeOutModel = () => (
+    <div className="popup-box" style={{background: '#403d3dcf'}}>
+            <div className='content session-timeout-model'>
+                <h4 className='text-danger'>Session Expired</h4>
+                <br />
+                <div>Your session ended because there was no activity.</div>
+                <div>Please login again.</div>
+                <br />
+                <div className='text-center'>
+                  <button className='btn btn-primary' onClick={gotoLoginPage}>Return Login Page</button>
+                </div>
+            </div>
+        </div>
+  )
+
   return (
+    <>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         {!isLogin && !isResetPassword && !isForgotPassword && _renderMainPage()}
@@ -84,6 +136,8 @@ const App = () => {
         {isForgotPassword && <ForgotPassword />}
       </PersistGate>
     </Provider>
+    {isShowIdleTimeOut && renderIdleTimeOutModel()}
+    </>
   );
 }
 
